f95782999a818666e924134fb60291210070d9dd
Masaya1582 <masa8y88@gmail.com>
2023-08-03 19:12:45 +0900

    DataTableの実装(How to use DataTable)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,88 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(TableViewApp());
 
-class MyApp extends StatelessWidget {
+class TableViewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      home: TableViewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class TableViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('TableView App'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            DataTable(
+              columns: [
+                DataColumn(label: Text('ID')),
+                DataColumn(label: Text('Name')),
+                DataColumn(label: Text('Age')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('1')),
+                  DataCell(Text('John')),
+                  DataCell(Text('28')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('2')),
+                  DataCell(Text('Alice')),
+                  DataCell(Text('25')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('3')),
+                  DataCell(Text('Bob')),
+                  DataCell(Text('32')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('4')),
+                  DataCell(Text('Kate')),
+                  DataCell(Text('23')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('5')),
+                  DataCell(Text('Michael')),
+                  DataCell(Text('35')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('6')),
+                  DataCell(Text('Emily')),
+                  DataCell(Text('29')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('7')),
+                  DataCell(Text('Daniel')),
+                  DataCell(Text('31')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('8')),
+                  DataCell(Text('Sophia')),
+                  DataCell(Text('27')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('9')),
+                  DataCell(Text('Henry')),
+                  DataCell(Text('24')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('10')),
+                  DataCell(Text('Olivia')),
+                  DataCell(Text('26')),
+                ]),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
