6bc55e8d0b8352ca556cf913c50d9dbd39486eac
Masaya1582 <masa8y88@gmail.com>
2023-08-06 10:41:42 +0900

    検索機能(SearchBar)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PokemonSearchApp());
 }
 
-class MyApp extends StatelessWidget {
+class PokemonSearchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      debugShowCheckedModeBanner: false,
+      home: PokemonSearchScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class PokemonSearchScreen extends StatefulWidget {
+  @override
+  _PokemonSearchScreenState createState() => _PokemonSearchScreenState();
+}
 
-  final String title;
+class _PokemonSearchScreenState extends State<PokemonSearchScreen> {
+  List<String> pokemons = [
+    'Bulbasaur',
+    'Charmander',
+    'Squirtle',
+    'Pikachu',
+    'Jigglypuff',
+    'Snorlax',
+    'Mewtwo',
+    'Gengar',
+    'Eevee',
+    'Dragonite',
+    'Mew',
+    'Rayquaza',
+  ];
+
+  List<String> filteredPokemons = [];
+
+  void searchPokemon(String value) {
+    filteredPokemons = pokemons
+        .where((pokemon) => pokemon.toLowerCase().contains(value.toLowerCase()))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Pokémon Search'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: searchPokemon,
+              decoration: InputDecoration(
+                hintText: 'Search Pokémon',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredPokemons.length,
+              itemBuilder: (context, index) {
+                final pokemon = filteredPokemons[index];
+                return ListTile(
+                  title: Text(pokemon),
+                  leading: CircleAvatar(
+                    child: Text(
+                      pokemon[0],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Colors.blue,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
