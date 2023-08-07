4b8e7d3e861d858a7fd5065b1c3bfc47c7e5fab3
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-07 15:54:51 +0900

    通過レートアプリ(Convert Currency)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,129 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(CurrencyConverterApp());
 }
 
-class MyApp extends StatelessWidget {
+class CurrencyConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Currency Converter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CurrencyConverterScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class CurrencyConverterScreen extends StatefulWidget {
+  @override
+  _CurrencyConverterScreenState createState() =>
+      _CurrencyConverterScreenState();
+}
 
-  final String title;
+class _CurrencyConverterScreenState extends State<CurrencyConverterScreen> {
+  double _amount = 1.0;
+  String _fromCurrency = 'USD';
+  String _toCurrency = 'EUR';
+  double _convertedAmount = 0.0;
+
+  Future<void> _convertCurrency() async {
+    final apiKey = 'YOUR API KEY'; // Replace with your actual API key
+    final apiUrl =
+        'https://data.fixer.io/api/convert?access_key=$apiKey&from=$_fromCurrency&to=$_toCurrency&amount=$_amount';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        _convertedAmount = data['result'];
+      });
+    } else {
+      // Handle error
+      print('Error: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Currency Converter'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                setState(() {
+                  _amount = double.tryParse(value) ?? 1.0;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                DropdownButton<String>(
+                  value: _fromCurrency,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _fromCurrency = newValue!;
+                    });
+                  },
+                  items: <String>['USD', 'EUR', 'GBP', 'JPY']
+                      .map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                ),
+                Icon(Icons.arrow_forward),
+                DropdownButton<String>(
+                  value: _toCurrency,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _toCurrency = newValue!;
+                    });
+                  },
+                  items: <String>['USD', 'EUR', 'GBP', 'JPY']
+                      .map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _convertCurrency,
+              child: Text('Convert'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Converted Amount: $_convertedAmount $_toCurrency',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
