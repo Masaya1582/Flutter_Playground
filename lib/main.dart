79aafd8c064d6f799fb77f1d7005250a6c82b7c8
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-08 07:19:38 +0900

    ランダム値の扱い方(Quote of the day)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(QuoteOfTheDayApp());
 }
 
-class MyApp extends StatelessWidget {
+class QuoteOfTheDayApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Quote of the Day',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: QuoteOfTheDayScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class QuoteOfTheDayScreen extends StatefulWidget {
+  @override
+  _QuoteOfTheDayScreenState createState() => _QuoteOfTheDayScreenState();
+}
 
-  final String title;
+class _QuoteOfTheDayScreenState extends State<QuoteOfTheDayScreen> {
+  final List<String> _quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+    // Add more quotes here
+  ];
+
+  String _currentQuote = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _currentQuote = _getRandomQuote();
+  }
+
+  String _getRandomQuote() {
+    final randomIndex = DateTime.now().day % _quotes.length;
+    return _quotes[randomIndex];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+      appBar: AppBar(title: Text('Quote of the Day')),
       body: Center(
-        child: Text('Hello World'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_currentQuote,
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic)),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _currentQuote = _getRandomQuote();
+                  });
+                },
+                child: Text('Next Quote'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
