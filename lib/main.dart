1024c3bc9d5cb0a617df636d43e97773fb9f0828
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-08 11:34:22 +0900

    RatingのUI(Star Rating)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(RatingApp());
 }
 
-class MyApp extends StatelessWidget {
+class RatingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Flutter Rating Bar App',
+      home: RatingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class RatingScreen extends StatefulWidget {
+  @override
+  _RatingScreenState createState() => _RatingScreenState();
+}
 
-  final String title;
+class _RatingScreenState extends State<RatingScreen> {
+  double _rating = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Flutter Rating Bar Example'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RatingBar.builder(
+              initialRating: _rating,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemSize: 50.0,
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {
+                setState(() {
+                  _rating = rating;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Rating: $_rating',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
       ),
     );
   }
