98f7d998aaf57fb9f48c462f4a961efee08a9098
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-09 09:08:50 +0900

    カート追加アニメーション(Add Cart Animation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(AddToCartAnimationApp());
 }
 
-class MyApp extends StatelessWidget {
+class Product {
+  final String name;
+  final double price;
+
+  Product(this.name, this.price);
+}
+
+class AddToCartAnimationApp extends StatelessWidget {
+  final List<Product> products = [
+    Product('Product 1', 10.0),
+    Product('Product 2', 20.0),
+    Product('Product 3', 30.0),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Add to Cart Animation',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ProductList(products: products),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ProductList extends StatelessWidget {
+  final List<Product> products;
 
-  final String title;
+  ProductList({required this.products});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+      appBar: AppBar(title: Text('Products')),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ProductItem(product: products[index]);
+        },
       ),
-      body: Center(
-        child: Text('Hello World'),
+    );
+  }
+}
+
+class ProductItem extends StatefulWidget {
+  final Product product;
+
+  ProductItem({required this.product});
+
+  @override
+  _ProductItemState createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  bool isAddingToCart = false;
+
+  void addToCart() {
+    setState(() {
+      isAddingToCart = true;
+    });
+
+    // Simulate an asynchronous process, e.g., adding to cart.
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        isAddingToCart = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.product.name),
+      subtitle: Text('\$${widget.product.price.toStringAsFixed(2)}'),
+      trailing: AnimatedSwitcher(
+        duration: Duration(milliseconds: 500),
+        child: isAddingToCart
+            ? Icon(Icons.check, color: Colors.green, key: UniqueKey())
+            : ElevatedButton(
+                onPressed: addToCart,
+                child: Text('Add to Cart'),
+              ),
       ),
     );
   }
