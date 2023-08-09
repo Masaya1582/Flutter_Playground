6c6abdc15dfd65846699c5d9281b6e8dbde32c53
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-09 09:25:54 +0900

    リストのアニメーション(List Animation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation_list/animation_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,25 +9,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Animation List Demo'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String title;
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Map<String, dynamic>> data = [
+    {
+      'title': '1111',
+      'backgroundColor': Colors.grey,
+    },
+    {
+      'title': '2222',
+      'backgroundColor': Colors.red,
+    },
+    {
+      'title': '3333',
+      'backgroundColor': Colors.yellow,
+    },
+    {
+      'title': '4444',
+      'backgroundColor': Colors.blue,
+    },
+    {
+      'title': '5555',
+      'backgroundColor': Colors.green,
+    },
+    {
+      'title': '6666',
+      'backgroundColor': Colors.orange,
+    },
+    {
+      'title': '7777',
+      'backgroundColor': Colors.brown,
+    },
+    {
+      'title': '8888',
+      'backgroundColor': Colors.purple,
+    },
+  ];
+
+  Widget _buildTile(String? title, Color? backgroundColor) {
+    return Container(
+        height: 100,
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          color: backgroundColor,
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
       body: Center(
-        child: Text('Hello World'),
+        child: AnimationList(
+            duration: 1500,
+            reBounceDepth: 30,
+            children: data.map((item) {
+              return _buildTile(item['title'], item['backgroundColor']);
+            }).toList()),
       ),
     );
   }
