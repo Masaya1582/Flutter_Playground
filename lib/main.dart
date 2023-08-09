c02a4d248c840b019b29a93e76cca819deb04dce
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-09 09:09:30 +0900

    Package版アニメーション

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,130 @@
+import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AddToCartAnimationApp());
+  runApp(const MyApp());
 }
 
-class Product {
-  final String name;
-  final double price;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  Product(this.name, this.price);
-}
-
-class AddToCartAnimationApp extends StatelessWidget {
-  final List<Product> products = [
-    Product('Product 1', 10.0),
-    Product('Product 2', 20.0),
-    Product('Product 3', 30.0),
-  ];
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Add to Cart Animation',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: ProductList(products: products),
+      title: 'Add To Cart Animation Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Add To Cart Animation Example'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class ProductList extends StatelessWidget {
-  final List<Product> products;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-  ProductList({required this.products});
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  // We can detect the location of the cart by this  GlobalKey<CartIconKey>
+  GlobalKey<CartIconKey> cartKey = GlobalKey<CartIconKey>();
+  late Function(GlobalKey) runAddToCartAnimation;
+  var _cartQuantityItems = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Products')),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ProductItem(product: products[index]);
-        },
+    return AddToCartAnimation(
+      // To send the library the location of the Cart icon
+      cartKey: cartKey,
+      height: 30,
+      width: 30,
+      opacity: 0.85,
+      dragAnimation: const DragToCartAnimationOptions(
+        rotation: true,
+      ),
+      jumpAnimation: const JumpAnimationOptions(),
+      createAddToCartAnimation: (runAddToCartAnimation) {
+        // You can run the animation by addToCartAnimationMethod, just pass trough the the global key of  the image as parameter
+        this.runAddToCartAnimation = runAddToCartAnimation;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: false,
+          actions: [
+            //  Adding 'clear-cart-button'
+            IconButton(
+              icon: const Icon(Icons.cleaning_services),
+              onPressed: () {
+                _cartQuantityItems = 0;
+                cartKey.currentState!.runClearCartAnimation();
+              },
+            ),
+            const SizedBox(width: 16),
+            AddToCartIcon(
+              key: cartKey,
+              icon: const Icon(Icons.shopping_cart),
+              badgeOptions: const BadgeOptions(
+                active: true,
+                backgroundColor: Colors.red,
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            )
+          ],
+        ),
+        body: ListView(
+          children: List.generate(
+            15,
+            (index) => AppListItem(
+              onClick: listClick,
+              index: index,
+            ),
+          ),
+        ),
       ),
     );
   }
-}
 
-class ProductItem extends StatefulWidget {
-  final Product product;
-
-  ProductItem({required this.product});
-
-  @override
-  _ProductItemState createState() => _ProductItemState();
-}
-
-class _ProductItemState extends State<ProductItem> {
-  bool isAddingToCart = false;
-
-  void addToCart() {
-    setState(() {
-      isAddingToCart = true;
-    });
-
-    // Simulate an asynchronous process, e.g., adding to cart.
-    Future.delayed(Duration(seconds: 1), () {
-      setState(() {
-        isAddingToCart = false;
-      });
-    });
+  void listClick(GlobalKey widgetKey) async {
+    await runAddToCartAnimation(widgetKey);
+    await cartKey.currentState!
+        .runCartAnimation((++_cartQuantityItems).toString());
   }
+}
 
+class AppListItem extends StatelessWidget {
+  final GlobalKey widgetKey = GlobalKey();
+  final int index;
+  final void Function(GlobalKey) onClick;
+
+  AppListItem({super.key, required this.onClick, required this.index});
   @override
   Widget build(BuildContext context) {
+    //  Container is mandatory. It can hold images or whatever you want
+    Container mandatoryContainer = Container(
+      key: widgetKey,
+      width: 60,
+      height: 60,
+      color: Colors.transparent,
+      child: Image.network(
+        "https://cdn.jsdelivr.net/gh/omerbyrk/add_to_cart_animation/example/assets/apple.png",
+        width: 60,
+        height: 60,
+      ),
+    );
+
     return ListTile(
-      title: Text(widget.product.name),
-      subtitle: Text('\$${widget.product.price.toStringAsFixed(2)}'),
-      trailing: AnimatedSwitcher(
-        duration: Duration(milliseconds: 500),
-        child: isAddingToCart
-            ? Icon(Icons.check, color: Colors.green, key: UniqueKey())
-            : ElevatedButton(
-                onPressed: addToCart,
-                child: Text('Add to Cart'),
-              ),
+      onTap: () => onClick(widgetKey),
+      leading: mandatoryContainer,
+      title: Text(
+        "Animated Apple Product Image $index",
       ),
     );
   }
