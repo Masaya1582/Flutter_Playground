c20350f33bc875c1f7353980f10b49c57e39c6f9
Masaya1582 <masa8y88@gmail.com>
2023-08-11 14:45:05 +0900

    Playサイコロ(Play Dice)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,106 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DiceRollerApp());
 }
 
-class MyApp extends StatelessWidget {
+class DiceRollerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Dice Roller App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DiceRollerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class DiceRollerScreen extends StatefulWidget {
+  @override
+  _DiceRollerScreenState createState() => _DiceRollerScreenState();
+}
 
-  final String title;
+class _DiceRollerScreenState extends State<DiceRollerScreen> {
+  int _dice1 = 1;
+  int _dice2 = 1;
+  bool _rolling = false;
+
+  void _rollDice() {
+    if (!_rolling) {
+      _rolling = true;
+      final random = Random();
+      Timer? rollingTimer;
+
+      rollingTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+        setState(() {
+          _dice1 = random.nextInt(6) + 1;
+          _dice2 = random.nextInt(6) + 1;
+        });
+
+        if (!_rolling) {
+          rollingTimer?.cancel();
+          setState(() {
+            // Optional: Generate a random number again to simulate the final result
+            _dice1 = random.nextInt(6) + 1;
+            _dice2 = random.nextInt(6) + 1;
+          });
+        }
+      });
+
+      Timer(Duration(seconds: 1), () {
+        setState(() {
+          _rolling = false;
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+      appBar: AppBar(title: Text('Dice Roller App')),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildDice(_dice1),
+                SizedBox(width: 16),
+                _buildDice(_dice2),
+              ],
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _rolling ? null : _rollDice,
+              child: Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDice(int value) {
+    return Container(
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: Text(
+          value.toString(),
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
