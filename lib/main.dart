a21cd40205db25bf97dc5ed42acb868341671802
Masaya1582 <masa8y88@gmail.com>
2023-08-11 19:16:40 +0900

    経費トラッカー(Expense Tracker)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,191 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ExpenseTrackerApp());
 }
 
-class MyApp extends StatelessWidget {
+class ExpenseTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ExpenseTrackerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ExpenseTrackerScreen extends StatefulWidget {
+  @override
+  _ExpenseTrackerScreenState createState() => _ExpenseTrackerScreenState();
+}
 
-  final String title;
+class _ExpenseTrackerScreenState extends State<ExpenseTrackerScreen> {
+  final List<Transaction> _transactions = [];
+
+  void _addTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      title: title, // Make sure 'title' is of type String
+      amount: amount, // Make sure 'amount' is of type double
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Expense Tracker'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            child: Card(
+              elevation: 5,
+              child: Text('Chart'),
+            ),
+          ),
+          TransactionList(_transactions),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Show a modal bottom sheet for adding a new transaction
+          showModalBottomSheet(
+            context: context,
+            builder: (_) {
+              return NewTransaction(_addTransaction);
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Transaction {
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.title, // Make sure 'title' is of type String
+    required this.amount, // Make sure 'amount' is of type double
+    required this.date,
+  });
+}
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$${tx.amount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tx.date), // Format the date
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class NewTransaction extends StatelessWidget {
+  final Function addTransaction;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTransaction);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTransaction(
+      enteredTitle, // Make sure 'enteredTitle' is of type String
+      enteredAmount, // Make sure 'enteredAmount' is of type double
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            controller: titleController,
+            onSubmitted: (_) => submitData(),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            controller: amountController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            onSubmitted: (_) => submitData(),
+          ),
+          ElevatedButton(
+            onPressed: submitData,
+            child: Text('Add Transaction'),
+          ),
+        ],
       ),
     );
   }
