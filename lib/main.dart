b725ec69010c23959da5e790aaed3617f57ad99c
Masaya1582 <masa8y88@gmail.com>
2023-08-11 15:06:24 +0900

    Contextを表示(Show Context Menu)

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ContextMenuApp());
 }
 
-class MyApp extends StatelessWidget {
+class ContextMenuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Context Menu Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ContextMenuScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class ContextMenuScreen extends StatelessWidget {
+  final List<String> itemList = ['Option 1', 'Option 2', 'Option 3'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+      appBar: AppBar(title: Text('Context Menu Example')),
+      body: ListView.builder(
+        itemCount: itemList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(itemList[index]),
+            onLongPress: () {
+              _showContextMenu(context, itemList[index]);
+            },
+          );
+        },
       ),
-      body: Center(
-        child: Text('Hello World'),
+    );
+  }
+
+  void _showContextMenu(BuildContext context, String item) {
+    final RenderBox overlay =
+        Overlay.of(context)!.context.findRenderObject() as RenderBox;
+
+    final relativePosition = RelativeRect.fromRect(
+      Rect.fromPoints(
+        overlay.localToGlobal(Offset.zero, ancestor: overlay),
+        overlay.localToGlobal(overlay.semanticBounds.bottomRight,
+            ancestor: overlay),
       ),
+      Offset.zero & overlay.semanticBounds.size,
+    );
+
+    showMenu(
+      context: context,
+      position: relativePosition,
+      items: <PopupMenuEntry>[
+        PopupMenuItem(
+          child: Text('Edit $item'),
+          onTap: () {
+            Navigator.pop(context);
+            // Perform edit action
+          },
+        ),
+        PopupMenuItem(
+          child: Text('Delete $item'),
+          onTap: () {
+            Navigator.pop(context);
+            // Perform delete action
+          },
+        ),
+      ],
     );
   }
 }
