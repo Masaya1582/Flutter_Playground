d7eb8328f3cfadde4228b14d2c6e07f497b58607
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-16 15:30:30 +0900

    DatePicker実装(DatePicker)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,74 @@
+import 'package:flutter/cupertino.dart'; // Import Cupertino package
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DatePickerApp());
 }
 
-class MyApp extends StatelessWidget {
+class DatePickerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      home: DatePickerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class DatePickerScreen extends StatefulWidget {
+  @override
+  _DatePickerScreenState createState() => _DatePickerScreenState();
+}
 
-  final String title;
+class _DatePickerScreenState extends State<DatePickerScreen> {
+  DateTime? selectedDate;
+
+  void _selectDate(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return Container(
+          height: 300,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.date,
+            initialDateTime: selectedDate ?? DateTime.now(),
+            minimumDate: DateTime(2000),
+            maximumDate: DateTime(2101),
+            onDateTimeChanged: (DateTime newDateTime) {
+              setState(() {
+                selectedDate = newDateTime;
+              });
+            },
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Date Picker App'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              selectedDate != null
+                  ? "${selectedDate!.toLocal()}".split(' ')[0]
+                  : 'Select a date',
+              style: TextStyle(fontSize: 55, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            CupertinoButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select date'),
+            ),
+          ],
+        ),
       ),
     );
   }
