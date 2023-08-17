89203d078d38ceb66a687aeb2de2d90eae9038d0
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-17 11:57:21 +0900

    Newsアプリ(News App with APIKey)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,70 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(NewsApp());
 }
 
-class MyApp extends StatelessWidget {
+class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NewsPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class NewsPage extends StatefulWidget {
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
 
-  final String title;
+class _NewsPageState extends State<NewsPage> {
+  final String apiKey = 'YOUR_API_KEY_HERE';
+  List<dynamic> articles = [];
+
+  Future<void> fetchNews() async {
+    final response = await http.get(Uri.parse(
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        articles = (data['articles'] as List).cast<
+            Map<String,
+                dynamic>>(); // Cast the list items to Map<String, dynamic>
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('News App'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: ListView.builder(
+        itemCount: articles.length,
+        itemBuilder: (context, index) {
+          final article = articles[index];
+          return ListTile(
+            title: Text(article['title'] as String), // Cast to String
+            subtitle:
+                Text(article['source']['name'] as String), // Cast to String
+          );
+        },
       ),
     );
   }
