e30a23220cea500bde149c95f9bd1567aaa9c47e
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-22 08:13:38 +0900

    PaymentFormView

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  runApp(MyApp());
+  runApp(PaymentApp());
 }
 
-class MyApp extends StatelessWidget {
+class PaymentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: 'Payment App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PaymentView(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class PaymentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Payment Details'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Enter Payment Information',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Card Number'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Card Holder Name'),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Expiry Date'),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Flexible(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'CVV'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add payment processing logic here
+                // For this example, let's just print a message
+                print('Payment Successful!');
+              },
+              child: Text('Make Payment'),
+            ),
+          ],
+        ),
       ),
     );
   }
