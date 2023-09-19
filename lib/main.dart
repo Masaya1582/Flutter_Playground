be89924727c4e31cb29fc0b255d22721db0767d4
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-19 10:17:36 +0900

    10 → 2進数Converter(中途半端)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(MyApp());
 }
 
@@ -11,27 +8,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      home: HomeView(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
 
-  final String title;
+class _HomeViewState extends State<HomeView> {
+  String numberString = "";
+  String result = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Decimal Number -> Binary Number"),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Result: $result", style: TextStyle(fontSize: 20)),
+            TextField(
+              decoration: InputDecoration(labelText: "Input Number"),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                setState(() {
+                  numberString = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                double number = double.tryParse(numberString) ?? 0.0;
+                setState(() {
+                  result = convert(number);
+                });
+              },
+              child: Text("Convert", style: TextStyle(color: Colors.white)),
+              style: ElevatedButton.styleFrom(primary: Colors.orange),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  String convert(double number) {
+    int integer = number.toInt();
+    double fraction = number - integer;
+    return '${intToBinaryNumberString(integer)}.${doubleToBinaryNumberString(fraction)}';
+  }
+
+  String intToBinaryNumberString(int number) {
+    List<int> binaryDigits = [];
+    while (number > 0) {
+      binaryDigits.add(number % 2);
+      number ~/= 2;
+    }
+    return binaryDigits.reversed.join();
+  }
+
+  String doubleToBinaryNumberString(double number) {
+    String result = "";
+    for (int i = 0; i < 6; i++) {
+      number *= 2;
+      result += number.toInt().toString();
+      number -= number.toInt();
+      if (number == 0) {
+        return result;
+      }
+    }
+    return result + "...";
   }
 }
