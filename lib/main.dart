07aa7ac30a4edd799f1095ed530a90ddf7411300
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-19 11:03:02 +0900

    iOSスタイルTextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,78 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+    return CupertinoApp(
+      title: 'CupertinoTextField Example',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  final String title;
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('CupertinoTextField Example'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CupertinoTextField(
+                controller: _textEditingController,
+                placeholder: 'Enter your text',
+                keyboardType: TextInputType.text,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: CupertinoColors.systemGrey,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              CupertinoButton.filled(
+                onPressed: () {
+                  final enteredText = _textEditingController.text;
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return CupertinoAlertDialog(
+                        title: Text('Entered Text'),
+                        content: Text(enteredText),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Show Text'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
