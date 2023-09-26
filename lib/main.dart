4c78bb1399726bcfb37d9b169146a8bf660c85de
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-26 13:16:02 +0900

    Todays Joke

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(MyApp());
 }
 
@@ -11,26 +11,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      title: "Today's Joke App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: JokeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class JokeScreen extends StatefulWidget {
+  @override
+  _JokeScreenState createState() => _JokeScreenState();
+}
 
-  final String title;
+class _JokeScreenState extends State<JokeScreen> {
+  String joke = "Press the button to get a joke";
+
+  Future<void> fetchJoke() async {
+    final response = await http
+        .get(Uri.parse("https://official-joke-api.appspot.com/jokes/random"));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final String setup = data['setup'] ?? "No setup available";
+      final String punchline = data['punchline'] ?? "No punchline available";
+      setState(() {
+        joke = "$setup\n\n$punchline";
+      });
+    } else {
+      setState(() {
+        joke = "Failed to load a joke";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Today's Joke"),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              joke,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: fetchJoke,
+              child: Text("Get Joke"),
+            ),
+          ],
+        ),
       ),
     );
   }
