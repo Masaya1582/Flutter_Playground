0088ab581c36b96e6ac5fc8d8eb3d8ee35219d1e
Masaya1582 <masa8y88@gmail.com>
2023-10-09 09:36:38 +0900

    IamgeをFetchする

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(MyApp());
 }
 
@@ -11,26 +9,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Hello World'),
+      home: ImageFetcherApp(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ImageFetcherApp extends StatefulWidget {
+  @override
+  _ImageFetcherAppState createState() => _ImageFetcherAppState();
+}
 
-  final String title;
+class _ImageFetcherAppState extends State<ImageFetcherApp> {
+  String imageUrl = '';
+
+  Future<void> fetchImage() async {
+    final response = await http.get(Uri.parse(
+        'https://masasophi.com/wp-content/uploads/2023/05/normal_swift.001.png'));
+    if (response.statusCode == 200) {
+      setState(() {
+        imageUrl = response.headers['location'] ?? '';
+      });
+    } else {
+      throw Exception('Failed to load image');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImage();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Image Fetcher App'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: imageUrl.isEmpty
+            ? CircularProgressIndicator()
+            : Image.network(
+                imageUrl,
+                width: 300,
+                height: 300,
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchImage,
+        tooltip: 'Fetch Image',
+        child: Icon(Icons.refresh),
       ),
     );
   }
